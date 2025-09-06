032ca3e47deb3a6f20c558bf144fd43c76603640
scokam <mishascokalo@gmail.com>
2025-09-05 21:18:23 -0400

    Basic setup

---
+++ ReadMe.txt
@@ -0,0 +1,11 @@
+**Environment setup:**
+
+I use conda to set up the environment:
+
+conda create -n game_env python=3.10
+
+conda activate game_env
+
+pip install pygame
+
+And now the environment should be all set up!
---
+++ main.py
@@ -0,0 +1,6 @@
+'''
+The main game loop. This is where objects are loaded, frames are updated and input
+is registered. Try to keep this as bare bones as possible so it runs smoothly. As many
+behaviours as possible should be contained in objects.
+
+'''
